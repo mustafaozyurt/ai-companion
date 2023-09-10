817f27382168f4db91abb34ab21f17639e27b853
mustafaozyurt <mustafaozyurt38@gmail.com>
2023-09-10 23:30:44 +0300

    categories added

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+import Categories from "@/components/categories";
 import SearchInput from "@/components/search-input";
 
-export default function RootPage() {
+export default async function RootPage() {
+
+  const db = new PrismaClient();
+
+  const categories = await db.category.findMany(); 
+
   return (
     <div className="relative">
       <div className="p-4 px-20">
-        <SearchInput/>
+        <SearchInput />
+        <Categories data={categories}/>
       </div>
     </div>
   );
